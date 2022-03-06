fe573455ccc357f8bb7148cb28e1c2ccc7c2bba8
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 02:11:47 -0700

    oneplus9: Update to Boot Control HAL v1.2
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I2e24de17d2a0cdc5d2622884ab76bf8ae6768964

--- device.mk
+++ device.mk
@@ -163,9 +163,9 @@ PRODUCT_COPY_FILES += \
 
 # Boot
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 # Camera
 $(call inherit-product-if-exists, vendor/oplus/camera/oplus-camera.mk)
