91260ce4068b2ea69fbaa9dbe492b83f2c5141b8
Alexander Koskovich <akoskovich@pm.me>
2022-05-18 05:29:56 -0700

    oneplus9: Pick up QSSI overlays from hardware/oplus
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I3eaf4720d5156dde3ae3b309e10fb40b3574eb11

--- device.mk
+++ device.mk
@@ -222,6 +222,8 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
 
 # Overlays
+$(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
+
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
