2d5a88f64ab7131db33adf7703c03522e2ea5da4
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2022-06-11 02:02:20 +0000

    oneplus9: Enable freeform windows
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ic1465ac678773e29fb561d0ea2ecc1ac1dd6b21f

--- device.mk
+++ device.mk
@@ -181,6 +181,10 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
