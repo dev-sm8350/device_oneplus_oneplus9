2b97332e003c4521d7d82a14f340b40d02466351
Michael Bestas <mkbestas@lineageos.org>
2023-05-04 01:19:43 +0300

    oneplus9: Build GPS HAL from source
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I4a205ef4b356f93379e592874d90bcfef6adb0d2

--- device.mk
+++ device.mk
@@ -195,6 +195,22 @@ PRODUCT_PACKAGES += \
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.gatekeeper.disable_spu=true
 
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti \
+    gnss@2.0-base.policy \
+    gnss@2.0-xtra-daemon.policy \
+    gnss@2.0-xtwifi-client.policy \
+    gnss@2.0-xtwifi-inet-agent.policy \
+    gps.conf \
+    flp.conf \
+    libbatching \
+    libgeofencing \
+    libgnss \
+    libgnsspps \
+    libloc_socket
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-qti \
