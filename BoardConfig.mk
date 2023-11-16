880347cdc3646772a72740e740fe4db6a48ba140
Bruno Martins <bgcngm@gmail.com>
2023-11-16 12:12:01 +0530

    oneplus9: Migrate to restructured sepolicy_vndr
    The new folder structure now looks like this:
    
    device/qcom/sepolicy_vndr
      ├─ legacy-um (meant for sdm845 until sm8350)
      ├─ sm8450
      └─ sm8550
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I432418a4241961a7f4c03b9f4a5b4a3618c78f5b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,6 +182,7 @@ SOONG_CONFIG_ufsbsg += ufsframework
 SOONG_CONFIG_ufsbsg_ufsframework := bsg
 
 # SELinux
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
