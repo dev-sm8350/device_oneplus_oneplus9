7094c776c4349e9119f02b4219811aab494df8da
TogoFire <togofire@mailfence.com>
2023-11-22 11:23:30 -0300

    oneplus9: products: Add support for OnePlus 9 Family
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I8de21eda6ea17136f22cd7f4835be36b6cf3b915

---
+++ lineage_oneplus9.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2021-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq (lineage_oneplus9,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the custom device configuration.
+$(call inherit-product, device/oneplus/oneplus9/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_BRAND := OnePlus
+PRODUCT_DEVICE := oneplus9
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MODEL := LE2115
+PRODUCT_NAME := lineage_oneplus9
+
+PRODUCT_SYSTEM_NAME := OnePlus9
+PRODUCT_SYSTEM_DEVICE := OnePlus9
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1080
+
+endif
