bb98ec16bd3d4f76fd6150708b7ec137df14c7a1
TogoFire <togofire@mailfence.com>
2023-11-22 11:23:30 -0300

    oneplus9: products: Add support for OnePlus 9 Family
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I13efb6994727f8dc2beb67168e3f8bbccdc8505f

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_oneplus9.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_oneplus9-user \
+    lineage_oneplus9-userdebug \
+    lineage_oneplus9-eng
