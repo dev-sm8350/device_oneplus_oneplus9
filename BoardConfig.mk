b93cc424821ebbfa8b26cb0565cf6617e67189e8
TogoFire <togofire@mailfence.com>
2023-11-22 23:22:15 -0300

    oneplus9: Introduced Lineage Health
    - Build Lineage Health HAL
    - Define proper charging control node (https://gitlab.com/LineageOS/issues/android/-/issues/5965)
    - Add chg_enable node to ueventd
    - Add sepolicy
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I2a7999ef5484ff67e896bc49039eb1c3ce8fcf9d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,6 +131,10 @@ KERNEL_DEFCONFIG := vendor/lahaina-qgki_defconfig
 USE_KERNEL_AOSP_LLVM := true
 KERNEL_FULL_LLVM := true
 
+# Lineage Health
+TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/chg_enable
+TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
+
 # OTA
 TARGET_OTA_ASSERT_DEVICE := OnePlus9|oneplus9|OnePlus9Pro|oneplus9pro
 
