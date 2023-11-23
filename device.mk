4d0ea39396a4797c1554f447749bad5387696763
ReallySnow <reallysnow233@gmail.com>
2023-11-24 01:10:32 +0800

    oneplus9: Build android.system.net.netd HIDL
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ib6b795daccd6d2f997baec247e841375305eb9e2

--- device.mk
+++ device.mk
@@ -292,6 +292,10 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/oplus
 
+# Netd
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.1.vendor
+
 # Neural Networks
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
