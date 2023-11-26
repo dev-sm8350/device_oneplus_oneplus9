3d7e5455c1adabdef15893936aa434d07e3981ef
TogoFire <togofire@mailfence.com>
2023-11-26 16:55:20 -0300

    oneplus9: Build Oplus Doze
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ia0edf6b32c3fd90a85d8139c9b8f6c6de26295f5

--- device.mk
+++ device.mk
@@ -169,6 +169,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oplus
 
+# Doze
+PRODUCT_PACKAGES += \
+    OplusDoze
+
 # Keymaster
 PRODUCT_PACKAGES += \
    android.hardware.keymaster@4.1.vendor
