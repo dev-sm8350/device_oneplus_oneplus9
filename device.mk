20e7912362b5f20aed18c276c135221802320518
Georg Veichtlbauer <georg@vware.at>
2023-12-09 16:40:36 +0100

    oneplus9: Inherit generic overlays from OPlus
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I44d5a851d4327df95ecbba4161471a42550dbd14

--- device.mk
+++ device.mk
@@ -313,6 +313,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.sys.radio.nrmode.auto=3
 
 # Overlays
+$(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
