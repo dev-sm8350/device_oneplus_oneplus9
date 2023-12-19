62e05d29895911cb29223fa719fed9615f2a8846
TogoFire <togofire@mailfence.com>
2023-12-19 09:23:42 -0300

    Revert "oneplus9pro: Build PowerShare."
    This reverts commit fd20974eb457509ac6bb80a534fe22934347f482.
    
    - Remove aospa stuff
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ie9446c2846cdc7c7e81126957eedbf6245c5b2ef

--- device.mk
+++ device.mk
@@ -140,7 +140,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     fstab.default \
-    init.oplus.charging.rc \
     init.oplus.display.rc \
     init.oplus.overlay.rc \
     init.oplus.perf.rc \
@@ -210,10 +209,6 @@ PRODUCT_COPY_FILES += \
 
 # Platform
 TARGET_BOARD_PLATFORM := lahaina
-
-# Powershare
-PRODUCT_PACKAGES += \
-    vendor.aospa.powershare-service
 
 # QTEE
 PRODUCT_PROPERTY_OVERRIDES += \
