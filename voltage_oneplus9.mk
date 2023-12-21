610eff0c69d992d5902888e40653b2c6259a5785
TogoFire <togofire@mailfence.com>
2023-12-21 17:58:07 -0300

    oneplus9: voltage-ify
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ie33cc602cce41d5ca8b7faa75f71b43f0541ea34

--- voltage_oneplus9.mk
+++ voltage_oneplus9.mk
@@ -1,10 +1,10 @@
 #
-# Copyright (C) 2021-2023 The LineageOS Project
+# Copyright (C) 2021-2023 The VoltageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-ifeq (lineage_oneplus9,$(TARGET_PRODUCT))
+ifeq (voltage_oneplus9,$(TARGET_PRODUCT))
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
@@ -13,14 +13,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from the custom device configuration.
 $(call inherit-product, device/oneplus/oneplus9/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 PRODUCT_BRAND := OnePlus
 PRODUCT_DEVICE := oneplus9
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := LE2115
-PRODUCT_NAME := lineage_oneplus9
+PRODUCT_NAME := voltage_oneplus9
 
 PRODUCT_SYSTEM_NAME := OnePlus9
 PRODUCT_SYSTEM_DEVICE := OnePlus9
@@ -33,5 +33,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 # Boot animation resolution.
 TARGET_BOOT_ANIMATION_RES := 1080
+
+# VoltageOS Stuffs
+VOLTAGE_BUILD_TYPE := OFFICIAL
 
 endif
