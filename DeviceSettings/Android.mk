3c4af6a806a9ecf8ea431b9c9416f59d1a43995f
TogoFire <togofire@mailfence.com>
2023-12-28 16:07:50 -0300

    Revert "oneplus9: Exclude org.lineageos.platform.internal dependency"
    This reverts commit 3af31f89adfbdef1267f82353aa6ed7824149507.
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I54028eca2f5d6e8588fc05bb141bbf78bb4e33a0

--- DeviceSettings/Android.mk
+++ DeviceSettings/Android.mk
@@ -37,6 +37,9 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
     androidx.preference_preference
 
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.lineageos.platform.internal
+
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res
 
