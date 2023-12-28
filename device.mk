2b7cbae967423bb23ef3d786da7d7283d6877476
TogoFire <togofire@mailfence.com>
2023-12-28 11:16:28 -0300

    oneplus9: set platform correctly
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Icc25c3c18602bf09172aa0a894f082c4c2f4d9e0

--- device.mk
+++ device.mk
@@ -405,9 +405,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
-# Platform
-TARGET_BOARD_PLATFORM := lahaina
-
 # Sensors
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
