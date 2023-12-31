4821f2d12d174150e20c7f7407fb52d759ab4f57
TogoFire <togofire@mailfence.com>
2023-12-31 18:51:41 -0300

    oneplus9: Use vendor fw compatibility matrix common
    - ref: https://github.com/LineageOS/android_device_oneplus_sm8350-common/blob/dc2e80170e5ca765ee3d01945650f1f9916da223/BoardConfigCommon.mk#L78
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ic3184850d20bf32a221073856333db9117411a30

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,7 +122,8 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/configs/vintf/oplus_vendor_framework_compatibility_matrix.xml
+    $(DEVICE_PATH)/configs/vintf/oplus_vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/manifest.xml
