a01198a889b9f69e4f524e363f3297c592a47908
Bruno Martins <bgcngm@gmail.com>
2024-03-28 14:12:52 +0000

    oneplus9: Mark CNE symlink target as required package
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ic3c1241346bcc94b84b81f7119a0dad5c0ebc00f

--- Android.mk
+++ Android.mk
@@ -66,13 +66,4 @@ $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
-CNE_LIBRARIES := libvndfwk_detect_jni.qti.so
-CNE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/CneApp/lib/arm64/,$(notdir $(CNE_LIBRARIES)))
-$(CNE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(CNE_SYMLINKS)
-
 endif
