21c25f90bee5826f892ce74b7c33160159fb7b17
Chirayu Desai <chirayudesai1@gmail.com>
2024-06-13 22:02:20 +0530

    oneplus9: Disable UFFD GC
    Unable to determine UFFD GC flag for kernel version "5.4.282-McQuaid".
    You can fix this by explicitly setting PRODUCT_ENABLE_UFFD_GC to "true" or
    "false" based on the kernel version.
    1. Set PRODUCT_ENABLE_UFFD_GC to "true" if the kernel supports userfaultfd(2)
       and MREMAP_DONTUNMAP.
    2. Set PRODUCT_ENABLE_UFFD_GC to "false" otherwise.
    
    Change-Id: Ie77d9a86a7fe94e4cade1868ffea27194f724e03
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- device.mk
+++ device.mk
@@ -335,6 +335,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oplus
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := false
+
 # Keymaster
 PRODUCT_PACKAGES += \
    android.hardware.keymaster@4.1.vendor
