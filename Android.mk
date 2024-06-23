731a564d2d0a7d2cae567870364b370c123af16e
Bruno Martins <bgcngm@gmail.com>
2024-06-23 15:42:45 +0100

    oneplus9: Declare IMS libs as symlinks during extraction
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Iafe06d9e166159e4fe3ee165e64acfda865f3211

--- Android.mk
+++ Android.mk
@@ -51,13 +51,4 @@ ALL_DEFAULT_INSTALLED_MODULES += \
 	$(EGL_32_SYMLINKS) \
 	$(EGL_64_SYMLINKS)
 
-IMS_LIBRARIES := libimscamera_jni.so libimsmedia_jni.so
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBRARIES)))
-$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /system/system_ext/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
-
 endif
