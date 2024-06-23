944d8dc7749ec6b50464780b4243a9bf911485b0
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    oneplus9: Move to new RFS install_symlink targets
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Iba665b1f254ce733e98477e2648033cd4eb0a433

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
