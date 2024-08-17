6fec67be3fd2169df55d44041315dbf21d3f4dd9
TogoFire <togofire@mailfence.com>
2024-08-17 03:37:54 -0300

    Revert "oneplus9: Cleanup in accordance to qcom common."
    This reverts commit 0e588154784c518fd9049f069c873db5bc77f56e.
    
    ref: https://github.com/ThankYouMario/proprietary_vendor_oneplus/commit/533adf40634a491e744625897418e1250ff34790
    
    Adapt to aosp.
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I18371de31964adeded0d03afba629c97dca9b8ca

--- device.mk
+++ device.mk
@@ -121,6 +121,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-service
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.memory.block@1.0.vendor
@@ -130,7 +135,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
 
 PRODUCT_PACKAGES += \
-    charger_fw_fstab.qti \
     fstab.default \
     init.oplus.charging.rc \
     init.oplus.display.rc \
@@ -139,6 +143,8 @@ PRODUCT_PACKAGES += \
     init.oplus.sensors.rc \
     init.oplus.telephony.rc \
     init.oplus.touch.rc \
+    init.oplus.wlan.rc \
+    init.qti.ufs.rc \
     init.target.rc \
     ueventd.oplus.rc
 
@@ -217,7 +223,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
     audio \
     av \
     bt \
-    charging \
     display \
     gps \
     init \
