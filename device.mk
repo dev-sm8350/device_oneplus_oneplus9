d52882301ef609a401aec58fbc57d5fe008b417a
TogoFire <togofire@mailfence.com>
2024-11-12 14:55:29 -0300

    Revert "oneplus9: Disable UFFD GC."
    This reverts commit 94e26e7c0756376be3746a9b47e18843384acd2c.
    
    Change-Id: b89a2b25f1646055f4993bd9ecdab36a416f1ba6
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- device.mk
+++ device.mk
@@ -141,9 +141,6 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.oplus.rc
 
-# Kernel
-PRODUCT_ENABLE_UFFD_GC := false
-
 # Keymaster
 PRODUCT_PACKAGES += \
    android.hardware.keymaster@4.1.vendor
